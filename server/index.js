5cf611676f2bca4e8da9969884a2257bf13e33bc
loyrZ <loyr22z@gmail.com>
2026-04-30 19:55:43 -0700

    added dem db

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const apiRoutes = require("./routes/api");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
@@ -24,6 +24,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Fair-lending tool running at http://localhost:${PORT}`);
 });
